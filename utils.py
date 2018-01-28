82a22f42f5ff36fe43c839c7f1e50e1267ce1427
qjy981010 <1114943038@qq.com>
2018-01-28 15:03:30 +0800

    save lab image in advance

--- utils.py
+++ utils.py
@@ -8,13 +8,37 @@ from torch.utils.data import DataLoader
 from torchvision import transforms
 
 
-def get_img_list(root, label_list, training):
+class LabTransformer(object):
+    """
+    """
+    def __init__(self, root):
+        self.root = root
+        self.crop = transforms.Compose([
+            transforms.Resize(256, Image.ANTIALIAS),
+            transforms.RandomCrop(224),
+        ])
+
+        srgb_profile = ImageCms.createProfile('sRGB')
+        lab_profile = ImageCms.createProfile('LAB')
+        self.labscale = ImageCms.buildTransformFromOpenProfiles(
+            srgb_profile, lab_profile, 'RGB', 'LAB')
+
+    def __call__(self, img_name):
+        img = Image.open(os.path.join(self.root, 'JPEGImages', img_name+'.jpg'))
+        img = self.crop(img)
+        lab_img = ImageCms.applyTransform(img, self.labscale)
+        pickle.dump(lab_img, os.join(self.root, img_name+'.pkl'),
+                    pickle.HIGHEST_PROTOCOL)
+
+
+def get_img_list(root, label_list, lab_transformer, training):
     result = []
     train_val = 'train' if training else 'val'
     pkl_file = os.path.join(root, 'img_list_%s.pkl'%(train_val, ))
     if os.path.exists(pkl_file):
         result = pickle.load(open(pkl_file, 'rb'))
     else:
+        os.system('rm '+os.path.join(root, '*.pkl'))
         label_encoder = {word: idx for idx, word in enumerate(label_list)}
         for file in os.listdir(root):
             filename = file[:-4].split('_')
@@ -22,10 +46,10 @@ def get_img_list(root, label_list, training):
                 continue
             with open(os.path.join(root, file), 'r') as fp:
                 for line in fp.readlines():
-                    result.append((label_encoder[filename[0]],
-                                   line.split()[0]))
-            break
-        pickle.dump(result, open(pkl_file, 'wb'), True)
+                    img_name = line.split()[0]
+                    result.append((label_encoder[filename[0]], img_name))
+                    lab_transformer(img_name)
+        pickle.dump(result, open(pkl_file, 'wb'), pickle.HIGHEST_PROTOCOL)
     return result
 
 
@@ -34,43 +58,24 @@ class Pascal(Dataset):
     """
     def __init__(self, root, label_list, training):
         super(Pascal, self).__init__()
+        lab_transformer = LabTransformer(os.path.join(root, 'JPEGImages'))
         self.img_list = get_img_list(os.path.join(root, 'ImageSets/Main'),
-                                     label_list, training)
-        self.root = root
-        self.crop = transforms.Compose([
-            transforms.Resize(256, Image.ANTIALIAS),
-            transforms.RandomCrop(224),
+                                     label_list, lab_transformer, training)
+        self.root = os.path.join(root, 'JPEGImages')
+
+        self.totensor = transforms.Compose([
             transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
         ])
-        self.grayscale = transforms.Grayscale()
-
-        srgb_profile = ImageCms.createProfile('sRGB')
-        lab_profile = ImageCms.createProfile('LAB')
-        self.labscale = ImageCms.buildTransformFromOpenProfiles(
-            srgb_profile, lab_profile, 'RGB', 'LAB')
-
-        self.totensor = transforms.ToTensor()
-        """
-        self.data = []
-        for label, img_name in self.img_list:
-            img = Image.open(os.path.join(self.root, 'JPEGImages', img_name+'.jpg'))
-            img = self.crop(img)
-            gray_img = self.totensor(self.grayscale(img))
-            lab_img = self.totensor(ImageCms.applyTransform(img, self.labscale))
-            self.data.append((gray_img, lab_img[1:, :, :], label))
-        """
 
     def __len__(self):
         return len(self.img_list)
 
     def __getitem__(self, idx):
         label, img_name = self.img_list[idx]
-        img = Image.open(os.path.join(self.root, 'JPEGImages', img_name+'.jpg'))
-        img = self.crop(img)
-        gray_img = self.totensor(self.grayscale(img))
-        lab_img = self.totensor(ImageCms.applyTransform(img, self.labscale))
-        return gray_img, lab_img[1:,:,:], label
-        #return self.data[idx]
+        img = pickle.load(open(os.path.join(self.root, img_name+'.pkl')), 'rb')
+        img = self.totensor(img)
+        return img[0, :, :], img[1:,:,:], label
 
 
 def load_data(root, label_list):
