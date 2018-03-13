3a9aa17b658f7ae80a6d03256f0320e5bf58f35f
qjy981010 <1114943038@qq.com>
2018-03-13 19:35:23 +0800

    sun

--- main.py
+++ main.py
@@ -3,7 +3,6 @@
 
 import torch
 import os
-import pickle
 import random
 import datetime
 import torch.optim as optim
@@ -14,31 +13,19 @@ from model import *
 from utils import *
 
 
-label_list = ['aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus',
-              'car', 'cat', 'chair', 'cow', 'dining', 'dog', 'horse',
-              'motorbike', 'person', 'pottedplant', 'sheep', 'sofa',
-              'train', 'tvmonitor']
-
-
-def train(dataloader, epoch_num, class_num,
-          net=None, lr=0.001, alpha=0.0033):
+def train(dataloader, iter_num, class_num, lr=1, alpha=0.0033,
+          log_iter=100, save_iter=500, model_path='colorization.pth'):
     """
     """
     use_cuda = torch.cuda.is_available()
-    if not net:
-        net = DataParallel(ColorizationNet(class_num))
 
-    ########
-    #for p in net.module.mid_level_net.parameters():
-    #    p.requires_grad = False
-    #for p in net.module.fusion.parameters():
-    #    p.requires_grad = False
-    #for p in net.module.colorization.parameters():
-    #    p.requires_grad = False
-    ########
+    net = ColorizationNet(class_num)
+    if os.path.exists(model_path):
+        net.load_state_dict(torch.load(model_path))
+
     classify_criterion = nn.CrossEntropyLoss()
     colorization_criterion = nn.MSELoss()
-    optimizer = optim.Adadelta(filter(lambda p: p.requires_grad, net.parameters()), lr=lr)# net.parameters(), lr=lr)
+    optimizer = optim.Adadelta(net.parameters(), lr=lr)
     if use_cuda:
         net = net.cuda()
         classify_criterion = classify_criterion.cuda()
@@ -46,15 +33,12 @@ def train(dataloader, epoch_num, class_num,
     else:
         print("*****   Warning: Cuda isn't available!  *****")
 
+    log_manager = LogManager(['class loss', 'color loss', 'total loss'],
+                             'log.txt', log_iter, save_iter, iter_num)
+
     print('====   Training..   ====')
-    start_epoch = get_start_epoch()
     net.train()
-    for epoch in range(start_epoch, start_epoch+epoch_num):
-        loss_sum = 0
-        color_loss_sum = 0
-        classify_loss_sum = 0
-        batch_loss = 0
-        start_time = datetime.datetime.now()
+    while True:
         for i, (gray_img, ab_img, label, posi) in enumerate(dataloader):
             if use_cuda:
                 gray_img = gray_img.cuda()
@@ -66,45 +50,41 @@ def train(dataloader, epoch_num, class_num,
 
             optimizer.zero_grad()
             classify_result, result = net(gray_img)
-            ###################
-            #x = net.module.low_level_net(gray_img)
-            #classify_input = net.module.global_level_net(x)[0]
-            #classify_result = net.module.classifier(classify_input)
-            ###################
-            posi = posi.nonzero().cuda()
-            if posi.max() > 0:
-                classify_result = classify_result[posi, :].squeeze(1)
-                label = label[[int(x) for x in posi]]
-                classify_loss = classify_criterion(classify_result, label)
-            else:
-                classify_loss = 0
             colorization_loss = colorization_criterion(result, ab_img)
-            #colorization_loss = 0
+            if posi[0] != -1:
+                # for pascal
+                posi = posi.nonzero().cuda()
+                if posi.max() > 0:
+                    classify_result = classify_result[posi, :].squeeze(1)
+                    label = label[[int(x) for x in posi]]
+                    classify_loss = classify_criterion(classify_result, label)
+                else:
+                    classify_loss = 0
+            else:
+                classify_loss = classify_criterion(classify_result, label)
             loss = colorization_loss + alpha * classify_loss
             loss.backward()
             optimizer.step()
 
-            loss_sum += loss
-            batch_loss += loss
-            color_loss_sum += colorization_loss
-            classify_loss_sum += classify_loss
-            if i%100 == 0 and i != 0:
-                print('batch: %d  loss: %f    ' % (i, batch_loss/100))
-                batch_loss = 0
-        end_time = datetime.datetime.now()
-        print('epoch: %d  classify loss: %f  color loss: %f  loss: %f  time: %s' %
-              (epoch, classify_loss_sum/(i+1), color_loss_sum/(i+1), loss_sum/(i+1), end_time-start_time))
-        train_log(epoch, classify_loss_sum/(i+1), color_loss_sum/(i+1), loss_sum/(i+1))
-        torch.save(net.state_dict(), 'colorization.pth')
-    print('Finished Training')
+            state = (log_manager.update([classify_loss.data[0], colorization_loss.data[0], loss.data[0]]))
+            if state == log_manager.SAVE:
+                print('saving...')
+                torch.save(net.state_dict(), model_path)
+                os.system('cp ' + model_path + ' ' + model_path[:-4] + '_copy.pth')
+            elif state == log_manager.EXIT:
+                return
 
 
-def test(dataloader, class_num, net, root):
+def test(dataloader, class_num, model_path, root):
     """
     """
     root = os.path.join(root, 'result')
     if not os.path.exists(root):
         os.makedirs(root)
+
+    net = ColorizationNet(class_num)
+    if os.path.exists(model_path):
+        net.load_state_dict(torch.load(model_path))
 
     use_cuda = torch.cuda.is_available()
     if use_cuda:
@@ -125,15 +105,18 @@ def test(dataloader, class_num, net, root):
         ab_img = Variable(ab_img)
 
         classify_result, result = net(gray_img)
-        posi = posi.nonzero()
-        if posi.max() > 0:
-            classify_result = classify_result[posi, :].squeeze(1)
-            label = label[posi, :].squeeze(1)
+        if posi[0] != -1:
+            posi = posi.nonzero()
+            if posi.max() > 0:
+                classify_result = classify_result[posi, :].squeeze(1)
+                label = label[posi, :].squeeze(1)
+                classify_result = torch.max(classify_result.data, 1)[1]
+                correct += (classify_result == label).sum()
+                total += label.size(0)
+        else:
             classify_result = torch.max(classify_result.data, 1)[1]
             correct += (classify_result == label).sum()
             total += label.size(0)
-        else:
-            classify_loss = 0
 
         save_img(root, i, result.data,
                  gray_img.data, ab_img.data)
@@ -141,24 +124,34 @@ def test(dataloader, class_num, net, root):
     test_log(correct / total)
 
 
-def main():
+def pascal_main():
     data_root = 'data/'
-    label_list = ['aeroplane', 'bicycle', 'bird', 'boat', 'bottle',
+    pascal_label_list = ['aeroplane', 'bicycle', 'bird', 'boat', 'bottle',
                   'bus', 'car', 'cat', 'chair', 'cow', 'diningtable',
                   'dog', 'horse', 'motorbike', 'person', 'pottedplant',
                   'sheep', 'sofa', 'train', 'tvmonitor']
-    trainloader, testloader = load_data(data_root, label_list)
-    net = DataParallel(ColorizationNet(len(label_list)))
+    trainloader, testloader = load_pascal(data_root, pascal_label_list)
+    net = DataParallel(ColorizationNet(len(pascal_label_list)))
 
     model_path = 'colorization.pth'
     if os.path.exists(model_path):
         net.load_state_dict(torch.load(model_path))
-    train(trainloader, 200, len(label_list), net=net, lr=1, alpha=1/300)
+    train(trainloader, 100, len(pascal_label_list), net=net, lr=1, alpha=1/300)
     #save_log()
     #torch.save(net.state_dict(), 'colorization.pth')
-    #test(testloader, len(label_list), net, data_root)
+    #test(testloader, len(pascal_label_list), net, data_root)
+
+
+def sun_main():
+    data_root = 'data/SUN'
+    pkl_root = 'data/SUNpkl'
+    loader = load_sun(data_root, pkl_root)
+    class_num = 362
+    net = ColorizationNet(class_num)
+    model_path = 'output/colorization.pth'
+    train(loader, 1000, class_num, lr=1, alpha=1/300, model_path=model_path)
 
 
 if __name__ == '__main__':
-    main()
+    sun_main()
 
--- utils.py
+++ utils.py
@@ -7,14 +7,81 @@ import matplotlib.pyplot as plt
 from PIL import Image, ImageCms
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
-from torchvision import transforms
+from torchvision import transforms, datasets
+
+
+class LogManager(object):
+    """
+    """
+    def __init__(self, loss_label, log_file, log_iter, save_iter, total_iter):
+        self.loss_label = loss_label
+        self.log_file = log_file
+        self.log_iter = log_iter
+        self.save_iter = save_iter
+        self.total_iter = total_iter
+
+        self.iter_counter = self.get_start_iter()
+        self.end_iter = self.iter_counter + total_iter
+        self.start_time = datetime.datetime.now()
+        self.loss = torch.zeros(len(loss_label))
+
+        self.UPDATE = 0
+        self.SAVE = 1
+        self.EXIT = 2
+
+    def get_start_iter(self):
+        if not os.path.exists(self.log_file):
+            os.mknod(self.log_file)
+        with open(self.log_file, 'r+') as fp:
+            lines = fp.readlines()
+            fp.write('start\n')
+            for i in range(1, len(lines)+1):
+                if lines[-i][:5] == 'saved':
+                    return int(lines[-i-1].split()[1])+1
+        return 0
+
+    def update(self, loss):
+        self.loss += torch.Tensor(loss)
+        self.iter_counter += 1
+
+        if self.iter_counter%self.log_iter == 0 and self.iter_counter != 0:
+            self.loss /= self.log_iter
+            losses = [str(loss) for loss in self.loss]
+            losses = (['iter %d'%(self.iter_counter)] +
+                      [': '.join(x) for x in zip(self.loss_label, losses)] +
+                      ['time: %s'%(datetime.datetime.now() - self.start_time)])
+            loss_log = '  '.join(losses)
+
+            print(loss_log)
+            with open(self.log_file, 'a') as fp:
+                fp.write(loss_log)
+                fp.write('\n')
+
+            self.loss.fill_(0)
+            self.start_time = datetime.datetime.now()
+
+            if self.iter_counter%self.save_iter == 0:
+                with open(self.log_file, 'a') as fp:
+                    fp.write('saved\n')
+                return self.SAVE
+        if self.iter_counter >= self.end_iter:
+            return self.EXIT
+        return self.UPDATE
+
+    def test(self, accuracy):
+        print('Classfication accuracy: %d %%' % (accuracy*100, ))
+        with open(log_file, 'a') as fp:
+            fp.write('Classfication accuracy: %d %%\n' % (accuracy*100, ))
 
 
 class LabSaver(object):
     """
     """
-    def __init__(self, root):
+    def __init__(self, root, save_root=None, name_ext='.pkl'):
         self.root = root
+        if save_root == None:
+            save_root = root
+        self.save_root = save_root
         self.crop = transforms.Compose([
             transforms.Resize(256, Image.ANTIALIAS),
         ])
@@ -25,74 +92,11 @@ class LabSaver(object):
             srgb_profile, lab_profile, 'RGB', 'LAB')
 
     def __call__(self, img_name):
-        img = Image.open(os.path.join(self.root, img_name+'.jpg'))
+        img = Image.open(os.path.join(self.root, img_name))
         img = self.crop(img)
         lab_img = ImageCms.applyTransform(img, self.labscale)
-        pickle.dump(lab_img, open(os.path.join(self.root, img_name+'.pkl'), 'wb'),
-                    pickle.HIGHEST_PROTOCOL)
-
-
-def get_img_list(root, label_list, lab_saver):
-    result = []
-    pkl_file = os.path.join(root, 'img_list.pkl')
-    if os.path.exists(pkl_file):
-        result = pickle.load(open(pkl_file, 'rb'))
-    else:
-        label_encoder = {word: idx for idx, word in enumerate(label_list)}
-        for file in os.listdir(root):
-            filename = file[:-4].split('_')
-            if len(filename) == 1 or filename[1] != 'trainval':
-                continue
-            label = label_encoder[filename[0]]
-            with open(os.path.join(root, file), 'r') as fp:
-                for line in fp.readlines():
-                    img_name, posi = line.split()
-                    result.append((label, img_name, posi == '1'))
-                    #lab_saver(img_name)
-        pickle.dump(result, open(pkl_file, 'wb'), pickle.HIGHEST_PROTOCOL)
-    return result
-
-
-class Pascal(Dataset):
-    """
-    """
-    def __init__(self, img_list, root):
-        super(Pascal, self).__init__()
-        self.img_list = img_list
-        self.root = os.path.join(root, 'JPEGImages')
-
-        self.totensor = transforms.Compose([
-            transforms.RandomCrop(224),
-            transforms.RandomHorizontalFlip(),
-            #transforms.CenterCrop(224),
-            transforms.ToTensor(),
-        ])
-
-    def __len__(self):
-        return len(self.img_list)
-
-    def __getitem__(self, idx):
-        label, img_name, posi = self.img_list[idx]
-        img = pickle.load(open(os.path.join(self.root, img_name+'.pkl'), 'rb'))
-        img = self.totensor(img)
-        return img[0].unsqueeze(0), img[1:], label, posi
-
-
-def load_data(root, label_list):
-    """
-    """
-    print('==== Loading data.. ====')
-    lab_saver = LabSaver(os.path.join(root, 'JPEGImages'))
-    img_list = get_img_list(os.path.join(root, 'ImageSets/Main'),
-                            label_list, lab_saver)
-
-    train_set = Pascal(img_list[:-10000], root)
-    test_set = Pascal(img_list[-10000:], root)
-    train_loader = DataLoader(train_set, batch_size=128,
-                              shuffle=True, num_workers=0)
-    test_loader = DataLoader(test_set, batch_size=64,
-                             shuffle=False, num_workers=0)
-    return train_loader, test_loader
+        img_name = img_name.split('.')[0]
+        pickle.dump(lab_img, open(os.path.join(self.save_root, img_name+name_ext), 'wb'))
 
 
 class LabTensorToRGB(object):
@@ -126,32 +130,117 @@ def save_img(root, i, out_ab_img, gray_img, ab_img):
         count += 1
 
 
-log_file = 'log.txt'
+def load_pascal(root, label_list):
+    """
+    """
+    print('==== Loading Pascal VOC.. ====')
+    lab_saver = LabSaver(os.path.join(root, 'JPEGImages'))
+    img_list = get_img_list(os.path.join(root, 'ImageSets/Main'),
+                            label_list, lab_saver)
 
-def train_log(epoch, classify_loss, color_loss, loss):
-    with open(log_file, 'a') as fp:
-        fp.write('epoch: %d  classify loss: %f  color loss: %f  loss: %f\n' %
-                 (epoch, classify_loss, color_loss, loss))
-
-
-def get_start_epoch():
-    if not os.path.exists(log_file):
-        os.mknod(log_file)
-    with open(log_file, 'r+') as fp:
-        lines = fp.readlines()
-        fp.write('start\n')
-        for i in range(1, len(lines)+1):
-            if lines[-i][:5] == 'epoch':
-                return int(lines[-i].split()[1])+1
-    return 0
+    train_set = Pascal(img_list[:-10000], root)
+    test_set = Pascal(img_list[-10000:], root)
+    train_loader = DataLoader(train_set, batch_size=32,
+                              shuffle=True, num_workers=0)
+    test_loader = DataLoader(test_set, batch_size=32,
+                             shuffle=False, num_workers=0)
+    return train_loader, test_loader
 
 
-def save_log():
-    with open(log_file, 'a') as fp:
-        fp.write('saved\n')
+# for Pascal VOC dataset
+def get_img_list(root, label_list, lab_saver):
+    result = []
+    pkl_file = os.path.join(root, 'img_list.pkl')
+    if os.path.exists(pkl_file):
+        result = pickle.load(open(pkl_file, 'rb'))
+    else:
+        label_encoder = {word: idx for idx, word in enumerate(label_list)}
+        for file in os.listdir(root):
+            filename = file[:-4].split('_')
+            if len(filename) == 1 or filename[1] != 'trainval':
+                continue
+            label = label_encoder[filename[0]]
+            with open(os.path.join(root, file), 'r') as fp:
+                for line in fp.readlines():
+                    img_name, posi = line.split()
+                    result.append((label, img_name, posi == '1'))
+                    lab_saver(img_name+'.jpg')
+        pickle.dump(result, open(pkl_file, 'wb'))
+    return result
 
 
-def test_log(accuracy):
-    with open(log_file, 'a') as fp:
-        fp.write('Classfication accuracy: %d %%\n' % (accuracy*100, ))
+class Pascal(Dataset):
+    """
+    """
+    def __init__(self, img_list, root):
+        super(Pascal, self).__init__()
+        self.img_list = img_list
+        self.root = os.path.join(root, 'JPEGImages')
 
+        self.totensor = transforms.Compose([
+            transforms.RandomCrop(224),
+            transforms.RandomHorizontalFlip(),
+            #transforms.CenterCrop(224),
+            transforms.ToTensor(),
+        ])
+
+    def __len__(self):
+        return len(self.img_list)
+
+    def __getitem__(self, idx):
+        label, img_name, posi = self.img_list[idx]
+        img = pickle.load(open(os.path.join(self.root, img_name+'pkl.jpg'), 'rb'))
+        img = self.totensor(img)
+        return img[0].unsqueeze(0), img[1:], label, posi
+
+
+def folder2lab(root, save_root):
+    if os.path.exists(save_root):
+        return
+    for folder in os.listdir(root):
+        img_path = os.path.join(root, folder)
+        save_path = os.path.join(save_root, folder)
+        os.makedirs(save_path)
+        lab_saver = LabSaver(img_path, save_path, name_ext='pkl.jpg')
+        for img in os.listdir(img_path):
+            lab_saver(img)
+
+
+def pkl_loader(file):
+    fp = open(file, 'rb')
+    result = pickle.load(fp)
+    fp.close()
+    return result
+
+
+class SunData(Dataset):
+    """
+    """
+    def __init__(self, root, save_root):
+        totensor = transforms.Compose([
+            transforms.RandomCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+        ])
+        folder2lab(root, save_root)
+        self.dataset = datasets.ImageFolder(root=save_root, transform=totensor,
+                                            loader=pkl_loader)
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, idx):
+        try:
+            img, label = self.dataset[idx]
+        except EOFError:
+            return 
+        return img[0].unsqueeze(0), img[1:], label, -1
+
+
+def load_sun(root, save_root):
+    """
+    """
+    print('==== Loading SUN dataset.. ====')
+    dataset = SunData(root, save_root)
+    loader = DataLoader(dataset, batch_size=28, shuffle=True, num_workers=4)
+    return loader
